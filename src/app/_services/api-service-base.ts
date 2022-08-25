d0e8b4447f56c568c446ba94b3ebf05b94621f2f
Philipp <philipp.henke98@outlook.de>
2022-08-25 21:41:33 +0200

    feat: extend api base

--- src/app/_services/api-service-base.ts
+++ src/app/_services/api-service-base.ts
@@ -2,8 +2,8 @@ import { HttpClient } from '@angular/common/http';
 import { BehaviorSubject, share } from 'rxjs';
 import { environment } from 'src/environments/environment';
 
-export class ApiServiceBase<T> {
-  constructor(private http: HttpClient, private endpointUrl: string) {
+export class ApiServiceBase<T extends { id: number }> {
+  constructor(protected http: HttpClient, private endpointUrl: string) {
     this.endpoint = environment.apiUrl + this.endpointUrl;
     this.loadAll();
   }
@@ -16,14 +16,54 @@ export class ApiServiceBase<T> {
 
   public create(element: T) {
     var request = this.http.post(this.endpoint, element);
-    request.subscribe((c) => {
+    const request$ = request.pipe(share());
+    request$.subscribe((data) => {
       this.loadAll();
     });
+    return request$;
+  }
+
+  public update(element: T) {
+    var request = this.http.put(this.endpoint, element);
+    const request$ = request.pipe(share());
+    request$.subscribe((data) => {
+      this.loadAll();
+    });
+    return request$;
   }
 
   public loadAll() {
     this.http
       .get<T[]>(this.endpoint)
       .subscribe((data) => this.subject.next(data));
+  }
+
+  public partialUpdate(element: Partial<T>) {
+    let patchValue = [];
+    for (const prop in element) {
+      if (prop === 'id') {
+        continue;
+      }
+
+      patchValue.push({
+        value: element[prop],
+        path: '/' + prop,
+        op: 'replace',
+      });
+    }
+
+    this.http
+      .patch(this.endpoint + '/' + element.id, patchValue)
+      .subscribe((data) => {
+        if (data) {
+          this.subject.next([
+            ...this.subject
+              .getValue()
+              .map((element: T) =>
+                element.id == (data as T).id ? (data as T) : element
+              ),
+          ]);
+        }
+      });
   }
 }
